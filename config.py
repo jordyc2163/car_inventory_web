b51b68cc7795ae075a48f63aaa72878756b6e661
jordyc2163 <jordycaceres00@gmail.com>
2022-06-29 01:52:12 -0400

    included forms and db

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
+from ntpath import join
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join('basedir', '.env'))
 # gives access to the project in any OS we find ourselves in
 # Allows outside files/folders to be added to the project from the base directory
 
 class Config():
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'SOMETHING'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATION = False #turns off updates messages from sqlalchemy
---
+++ hw_app/forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from marshmallow import ValidationError
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Email
+
+class UserSignUpForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    age = IntegerField('Age')
+    submit_button = SubmitField()
+
+    def validate_age(form, field):
+        if field.data < 18:
+            raise ValidationError('You must be 18 years or older to purchase a car.')
+
+class UserSignInForm(FlaskForm):
+    # email, password, submit
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit_button = SubmitField()
